baa1549b594b55162060f7a24a41a9292761ef1f
Muzammil-haider <muzammilhaider17@gmail.com>
2025-01-11 15:29:38 +0500

    add project-02

---
+++ my-app-03/src/app/page.tsx
@@ -0,0 +1,18 @@
+import Header from "@/components/Header";
+import HeroSection from "@/components/HeroSection";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <HeroSection />
+    
+    </div>
+  );
+}
+
+
+
+
+
+
